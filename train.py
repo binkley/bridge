e878a94914d7b178fb4b7df6656c98012d490be7
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2024-12-11 08:29:14 -0600

    Failed test on purpose

--- train.py
+++ train.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
 import torch
-from datasets import load_dataset
 from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments
 from trl import SFTTrainer
+
+def train(path):
+    """Return the trained dataset.
+
+    >>> train('data/tatsu-lab/alpacka')
+    """
+    from datasets import load_dataset
+    return load_dataset(path, split='train')
 
 if __name__ == '__main__':
     import doctest
